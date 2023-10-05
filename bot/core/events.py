b1ac595542e96fe31e67aa3dabd4ee638178947f
MakinoHaruka <harukaxxxx@makinoworks.com>
2023-10-05 21:35:57 +0800

    ✨ feat(bot&util): main function
    setup cielifra main function to read PNG parameters and DM to user

--- bot/core/events.py
+++ bot/core/events.py
@@ -8,8 +8,18 @@ from rich.box import MINIMAL
 from rich.columns import Columns
 from rich.panel import Panel
 from rich.table import Table
+import io
+from PIL import Image
 
-from bot import ApplicationContext, BaseCog, Bot, Translator
+from bot import (
+    ApplicationContext,
+    BaseCog,
+    Bot,
+    Translator,
+    generate_spell,
+    extra_parameter,
+    split_prompt,
+)
 
 _ = Translator(__name__)
 
@@ -32,9 +42,7 @@ class BaseEventsCog(BaseCog):
         for cog in bot.cogs.values():
             table_cogs_info.add_row(
                 cog.__cog_name__,
-                f"{docs[:30]}..."
-                if len(docs := cog.__cog_description__) > 20
-                else docs or "-",
+                f"{docs[:30]}..." if len(docs := cog.__cog_description__) > 20 else docs or "-",
             )
 
         table_general_info = Table(show_edge=False, show_header=False, box=MINIMAL)
@@ -98,6 +106,163 @@ class BaseEventsCog(BaseCog):
         error: DiscordException,
     ):
         self.log.exception(type(error).__name__, exc_info=error)
+
+    @discord.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        TRIGGER_REACTION = "🤩"
+        bot = self.bot
+        if payload.emoji.name == TRIGGER_REACTION:
+            reaction_member = payload.member
+            channel = bot.get_channel(payload.channel_id)
+            channel_id = payload.channel_id
+            channel_name = channel.name
+            message = await channel.fetch_message(int(payload.message_id))
+            message_link = message.jump_url
+            guild = bot.get_guild(payload.member.guild.id)
+            guild_name = guild.name
+            guild_icon_url = (
+                guild.icon.url
+                if guild.icon != None
+                else f"https://fakeimg.pl/300x300/?text={guild_name[0]}"
+            )
+            author = await message.guild.fetch_member(message.author.id)
+            author_name = author.nick if author.nick != None else author.name
+            author_avatar_url = (
+                author.avatar.url
+                if author.avatar != None
+                else f"https://fakeimg.pl/300x300/?text={author_name[0]}"
+            )
+
+            attachments = message.attachments
+
+            if attachments:
+                for attachment in attachments:
+                    attachment_id = f"{channel_id}-{attachment.id}"
+                    # Check if the attachment is a PNG file
+                    if attachment.filename.endswith(".png"):
+                        # Read attachment data into memory
+                        image_data = io.BytesIO(await attachment.read())
+
+                        # Open the image from memory
+                        with Image.open(image_data) as img:
+                            img.load()
+                            try:
+                                parameter = img.info["parameters"]
+                            except KeyError:
+                                # Parameters not found
+                                await message.remove_reaction(TRIGGER_REACTION, reaction_member)
+                                await message.add_reaction("❎")
+                                continue
+
+                    else:
+                        # Not a PNG file
+                        await message.remove_reaction(TRIGGER_REACTION, reaction_member)
+                        await message.add_reaction("❎")
+                        return
+
+                    # Process parameter
+                    nprompt_index = parameter.find("Negative prompt: ")
+                    steps_index = parameter.find("Steps: ")
+                    prompts = parameter[0:nprompt_index]
+                    nprompts = parameter[nprompt_index + 17 : steps_index]
+                    extras = parameter[steps_index : len(parameter)]
+
+                    # Building DM embed message
+                    dm_channel = await reaction_member.create_dm()
+                    title = f"『{generate_spell(attachment_id)}』"
+                    color = discord.Colour.from_rgb(127, 0, 32)
+                    embed = discord.Embed(
+                        title=title, description="", colour=color, url=message_link
+                    )
+
+                    # Setup prompt field
+                    prompt_list = split_prompt(prompts)
+                    if len(prompt_list) > 1:
+                        for i, prompt in enumerate(prompt_list, 1):
+                            embed.add_field(name=f"Prompt {i}", value=prompt, inline=False)
+                    else:
+                        embed.add_field(name="Prompt", value=prompts, inline=False)
+
+                    # Setup negtive prompt field
+                    nprompt_list = split_prompt(nprompts)
+                    if len(nprompt_list) > 1:
+                        for i, nprompt in enumerate(nprompt_list, 1):
+                            embed.add_field(
+                                name=f"Negative Prompt {i}", value=nprompt, inline=False
+                            )
+                    else:
+                        embed.add_field(name="Negative Prompt", value=nprompts, inline=False)
+
+                    # Setup parameter field
+                    parameter_fields = [
+                        "Steps",
+                        "CFG scale",
+                        "Seed",
+                        "Sampler",
+                        "Model",
+                        "Model hash",
+                        "Size",
+                        "Version",
+                        "Hires upscale",
+                        "Hires steps",
+                        "Hires upscaler",
+                        "Denoising strength",
+                    ]
+                    embed.add_field(name="Parameters", value="", inline=False)
+                    for field in parameter_fields:
+                        # check if Hires exist print hires info else break loop
+                        if field == "Hires upscale" and parameter.find("Hires upscale: ") < 0:
+                            break
+                        elif field == "Hires upscale":
+                            embed.add_field(name="Hires info", value="", inline=False)
+                        embed.add_field(
+                            name=field,
+                            value=extra_parameter(parameter, f"{field}: "),
+                        )
+
+                    # Setup extra field
+                    for field in parameter_fields:
+                        extras = extras.replace(
+                            f'{field}: {extra_parameter(parameter,f"{field}: ")}, ',
+                            "",
+                        )
+                        extras = extras.replace(
+                            f'{field}: {extra_parameter(parameter,f"{field}: ")}',
+                            "",
+                        )
+                    extra_list = split_prompt(extras)
+                    if len(extra_list) > 1:
+                        for i, extra in enumerate(extra_list, 1):
+                            embed.add_field(name=f"extra info {i}", value=extra, inline=False)
+                    else:
+                        embed.add_field(name="extra info", value=extras, inline=False)
+
+                    # Setup other embed elements
+                    embed.set_image(url=attachment.url)
+                    embed.set_author(name=author_name, icon_url=author_avatar_url)
+                    embed.set_footer(
+                        text=f"from「{guild_name}」{channel_name}channel",
+                        icon_url=guild_icon_url,
+                    )
+
+                    # Send DM
+                    try:
+                        await dm_channel.send(embed=embed)
+                        print("normal sent")
+                    except discord.errors.HTTPException as exception_occurred:
+                        if exception_occurred.status == 400 and exception_occurred.code == 50035:
+                            await dm_channel.send(
+                                content=f"Infinite Magic Projection has exceeded its capacity, and Cielifra is running out of mana!\nPlease download the PNG manually and place it in the sd-webui PNG INFO tab to obtain the parameters.\nThe original link to the message is:{message_link}"
+                            )
+                            await message.remove_reaction(TRIGGER_REACTION, reaction_member)
+                            await message.add_reaction("❎")
+                            print("400 and 50035 error", exception_occurred)
+                        else:
+                            self.log.exception("An HTTPException occurred:", exception_occurred)
+                            print("error")
+                    self.log.info(f"DM sent to {reaction_member.name}.")
+            else:
+                await message.remove_reaction(TRIGGER_REACTION, reaction_member)
 
 
 def setup(bot: "Bot"):
--- bot/utils/util.py
+++ bot/utils/util.py
@@ -1,9 +1,18 @@
 import inspect
+import hashlib
+import random
+import math
+import os
+import json
 from pathlib import Path
 
 __all__ = (
     "fix_doc",
     "get_absolute_name_from_path",
+    "generate_spell",
+    "extra_parameter",
+    "split_prompt",
+    "load_guild_params",
 )
 
 
@@ -46,3 +55,121 @@ def get_absolute_name_from_path(
         paths.append(p.stem)
 
     return ".".join(reversed(paths))
+
+
+def generate_spell(input_text):
+    magic_spells = [
+        "⚡✨Zephyrus Alakazamia✨⚡",
+        "🌟✨Lumina Seraphicus✨🌟",
+        "🌌🌟Astra Eternus🌟🌌",
+        "🌙✨Mysticus Vorpalus✨🌙",
+        "🔥✨Solarius Incantatio✨🔥",
+        "🌪️✨Aquilo Spiralis✨🌪️",
+        "🐉🔥Ignis Draconis🔥🐉",
+        "🌿🌳Veridia Arborum🌳🌿",
+        "🌌⭐Celestis Mirabilis⭐🌌",
+        "🌑🌙Umbra Nocturna🌙🌑",
+        "✨⚡Divinus Fulgor⚡✨",
+        "🌧️🌊Tempestas Fluvius🌊🌧️",
+        "🌍🌱Terra Vitalis🌱🌍",
+        "🦅✨Volatus Levitas✨🦅",
+        "🔮✨Arcanus Omnipotens✨🔮",
+        "💜✨Amethysta Magica✨💜",
+        "🛡️✨Fortis Protego✨🛡️",
+        "❓🌌Enigma Invisus🌌❓",
+        "⚔️🌌Bellum Caelum🌌⚔️",
+        "🌳🌿Sylva Perpetua🌿🌳",
+        "🔥🌌Flamara Infernalis🌌🔥",
+        "✨🌌Aetherius Radiance🌌✨",
+        "🌐🔗Meridianus Nexus🔗🌐",
+        "🌈🌌Spectra Illusionis🌌🌈",
+        "🌌🔮Nexus Portentia🔮🌌",
+        "🕊️🌬️Volucris Velocitas🌬️🕊️",
+        "🔮🌌Mirus Transmutatio🌌🔮",
+        "⚡✨Fulgurante Lumine✨⚡",
+        "🌺🌟Harmonia Elysium🌟🌺",
+        "🌌✨Luminara Effervescens✨🌌",
+        "🌸✨Flora Viventia✨🌸",
+        "🔥🌪️Ignis Turbinis🌪️🔥",
+        "🌟🌌Stellae Infinitas🌌🌟",
+        "⚡🌊Fulgor Aqua🌊⚡",
+        "🌑🦉Umbra Noctua🦉🌑",
+        "🌞🌙Lux Lunaris🌙🌞",
+        "🍃✨Aura Vitalis✨🍃",
+        "🔮🌌Magia Arcana🌌🔮",
+        "🌹✨Rosaceus Lumina✨🌹",
+        "🌌🗝️Cosmos Clavis🗝️🌌",
+        "🌪️🌊Tempestas Mare🌊🌪️",
+        "🔥⚔️Ignis Gladius⚔️🔥",
+        "🌈🔮Iris Divinatio🔮🌈",
+        "💫✨Siderea Splendor✨💫",
+        "🌙🦋Noctis Papilio🦋🌙",
+        "🔥🌿Ignis Herba🌿🔥",
+        "⚡🌪️Fulgur Turbo🌪️⚡",
+        "🌌✨Astrum Luminis✨🌌",
+        "🌺🌊Flora Maris🌊🌺",
+        "🌙🔮Luna Divinatrix🔮🌙",
+        "🌟🌿Stella Viridis🌿🌟",
+    ]
+
+    # Convert to MD5 hash value
+    md5_hash = hashlib.md5(input_text.encode()).hexdigest()
+
+    # Convert MD5 hash value to integer
+    hash_int = int(md5_hash, 16)
+
+    # Randomly choose a spell
+    random.seed(hash_int)
+    random_spell = random.choice(magic_spells)
+
+    return random_spell
+
+
+def extra_parameter(parameter, scope):
+    start_index = parameter.find(scope)
+    if start_index > 0:
+        parameter_length = parameter[start_index : len(parameter)].find(",")
+        if parameter_length < 0:
+            parameter_length = len(parameter)
+        return parameter[start_index + len(scope) : start_index + parameter_length]
+    else:
+        return "-"
+
+
+def split_prompt(prompt):
+    if len(prompt) > 1024:
+        parts = prompt.split(",")
+        result_list = []
+        temp = ""
+
+        slice_num = len(prompt) / math.ceil(len(prompt) / 1024)
+        if slice_num * 1.1 >= 1024:
+            slice_num = 1024
+        else:
+            slice_num = slice_num * 1.1
+
+        for part in parts:
+            if len(temp) + len(part) <= slice_num:
+                temp += part + ","
+            else:
+                result_list.append(temp)
+                temp = part + ","
+        if temp:
+            result_list.append(temp)
+    else:
+        result_list = [prompt]
+
+    return result_list
+
+
+def load_guild_params(guild_id, param_id):
+    guild_config_path = "./config/guild_config.json"
+    default_config_path = "./config/config.json"
+    if guild_id == "default_guild" or not os.path.exists(guild_config_path):
+        with open(default_config_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            return data[guild_id][param_id]
+    else:
+        with open(guild_config_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            return data[guild_id][param_id]
